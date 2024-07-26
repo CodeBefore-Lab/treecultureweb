7de6f66881430029e37b2b39a4c1b1af67e1983f
mrtlive <mrtlive1@gmail.com>
2024-07-26 11:50:56 +0300

    chore: Update docker image version to 0.0.3 and change backend URL

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,7 +2,8 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://api.pestomat.com/v1/treewebapi/"; // Update this with your backend URL
+// const BASE_URL = "https://api.pestomat.com/v1/treewebapi/"; // Update this with your backend URL
+const BASE_URL = "http://160.20.111.43:3006/api/";
 
 async function sendRequest(method, url, data = null, options = {}, token = null) {
   // Get the token from local storage without ""
